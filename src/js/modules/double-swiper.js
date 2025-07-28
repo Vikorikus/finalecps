6eecc2b7daa05b25fe36189510a628b30087ac3b
Vikorikus <Vikorikus@gmail.com>
2025-07-28 13:49:50 +0300

    точки свайпера убрать выше 768px

--- src/js/modules/double-swiper.js
+++ src/js/modules/double-swiper.js
@@ -1,27 +1,22 @@
 let brandsSwiper = null
 let techniqueSwiper = null
-
+const swiperPagination = document.querySelector('.swiper-pagination')
 function initSwipers() {
-  // НАСТРОЙКИ для всех свайперов
-  const swiperSettings = {
-    slidesPerView: 1.3,
-    spaceBetween: 0,
-    breakpoints: {
-      400: { slidesPerView: 1.6 },
-      500: { slidesPerView: 1.8 },
-      600: { slidesPerView: 2.2 },
-      700: { slidesPerView: 2.5 },
-      767: { slidesPerView: 2.8 },
-    },
-  }
-
   // === Свайпер брендов ===
   const brandsContainer = document.querySelector('.brands__swiper')
   if (brandsContainer) {
     if (window.innerWidth <= 768) {
       if (!brandsSwiper) {
         brandsSwiper = new Swiper('.brands__swiper', {
-          ...swiperSettings,
+          slidesPerView: 1.3,
+          spaceBetween: 0,
+          breakpoints: {
+            400: { slidesPerView: 1.6 },
+            500: { slidesPerView: 1.8 },
+            600: { slidesPerView: 2.2 },
+            700: { slidesPerView: 2.5 },
+            767: { slidesPerView: 2.8 },
+          },
           pagination: {
             el: '.brands__pagination',
             clickable: true,
@@ -32,6 +27,7 @@ function initSwipers() {
       if (brandsSwiper) {
         brandsSwiper.destroy(true, true)
         brandsSwiper = null
+        swiperPagination.innerHTML = ''
       }
     }
   }
@@ -42,7 +38,15 @@ function initSwipers() {
     if (window.innerWidth <= 768) {
       if (!techniqueSwiper) {
         techniqueSwiper = new Swiper('.repair-technique__swiper', {
-          ...swiperSettings,
+          slidesPerView: 1.3,
+          spaceBetween: 0,
+          breakpoints: {
+            400: { slidesPerView: 1.6 },
+            500: { slidesPerView: 1.8 },
+            600: { slidesPerView: 2.2 },
+            700: { slidesPerView: 2.5 },
+            767: { slidesPerView: 2.8 },
+          },
           pagination: {
             el: '.repair-technique__pagination',
             clickable: true,
@@ -53,6 +57,7 @@ function initSwipers() {
       if (techniqueSwiper) {
         techniqueSwiper.destroy(true, true)
         techniqueSwiper = null
+        swiperPagination.innerHTML = ''
       }
     }
   }
