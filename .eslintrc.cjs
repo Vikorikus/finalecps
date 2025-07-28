ec1d43d00028e4888739e496b97c198aacbb30db
Vikorikus <Vikorikus@gmail.com>
2025-07-28 12:54:19 +0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    // свои правила тут
+  },
+};
